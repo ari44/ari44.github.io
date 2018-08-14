ff97a13ba7a419ef8ef640fcbc660a3d1cadc920
ariyoshi <ariyoshi@kdl.co.jp>
2018-08-14 15:29:00 +0900

    b

--- serviceworker.js
+++ serviceworker.js
@@ -5,9 +5,10 @@ self.addEventListener('install', function(event) {
 });
 
 self.addEventListener('push', function(event) {
+    const message = event.data ? event.data.text() : '(ÅEÅÕÅE)';
     event.waitUntil(
         self.registration.showNotification('Push Received', {
-            body: 'Push Notification Received',
+            body: message,
             tag: 'push-notification-tag' 
         })
     );
